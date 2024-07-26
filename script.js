f466bc26f6afe9184c84ab10621444d842fa12fe
Cameron Barr <cjbarr1013@gmail.com>
2024-07-25 21:13:38 -0400

    Create files and add basic library functionality
    Created base files for library page and added basic functionality to
    take user input and add book to array.

---
+++ script.js
@@ -0,0 +1,19 @@
+const myLibrary = [];
+
+function Book(author, title, pages, read) {
+  this.author = author,
+  this.title = title,
+  this.pages = pages,
+  this.read = read
+};
+
+function addBookToLibrary() {
+  let author = prompt("Author?");
+  let title = prompt("Title?");
+  let pages = prompt("Pages? (must be a positive integer)");
+  let read = prompt("Have you read it? (y/n)");
+  myLibrary.push(new Book(author, title, pages, read));
+};
+
+const myButton = document.querySelector("button");
+myButton.addEventListener("click", addBookToLibrary);
