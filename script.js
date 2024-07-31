e3772f83e802fd3ab353708065d49327da85c699
Cameron Barr <cjbarr1013@gmail.com>
2024-07-30 20:33:06 -0400

    Fix add book issue where books repeat
    Fixed displayBook function so the entire library isn't looped through
    when trying to display a new book that has been added

--- script.js
+++ script.js
@@ -1,19 +1,78 @@
-const myLibrary = [];
+let myLibrary = [
+  new Book("George R.R. Martin", "A Game of Thrones", 1088, "Yes", "Great book, would highly recommend!"),
+  new Book("Adam Savage", "Every Tool's a Hammer", 320, "No", ""),
+  new Book("Tom Doak", "Anatomy of a Golf Course", 288, "Yes", "Excellent look at how a golf course is made.")
+];
 
-function Book(author, title, pages, read) {
+const myButton = document.querySelector("#add");
+myButton.addEventListener("click", () => {
+  const newBook = addBookToLibrary();
+  displayBook(newBook);
+});
+
+const tableBody = document.querySelector("tbody");
+
+function Book(author, title, pages, read, comments) {
   this.author = author,
   this.title = title,
   this.pages = pages,
   this.read = read
+  this.comments = comments;
 };
 
 function addBookToLibrary() {
-  let author = prompt("Author?");
-  let title = prompt("Title?");
-  let pages = prompt("Pages? (must be a positive integer)");
-  let read = prompt("Have you read it? (y/n)");
-  myLibrary.push(new Book(author, title, pages, read));
+  const author = prompt("Author?");
+  const title = prompt("Title?");
+  const pages = prompt("Pages? (must be a positive integer)");
+  const read = prompt("Have you read it? (y/n)");
+  const comments = prompt("Any comments?");
+  
+  const newBook = new Book(author, title, pages, read, comments)
+  myLibrary.push(newBook);
+  return newBook;
 };
 
-const myButton = document.querySelector("#add");
-myButton.addEventListener("click", addBookToLibrary);
+function displayBook(book) {
+  // Create elements and add classes to buttons
+  const row = document.createElement("tr");
+
+  const author = document.createElement("td");
+  author.textContent = book.author;
+
+  const title = document.createElement("td");
+  title.textContent = book.title;
+
+  const pages = document.createElement("td");
+  pages.textContent = book.pages;
+
+  const read = document.createElement("td");
+  read.textContent = book.read;
+
+  const comments = document.createElement("td");
+  comments.textContent = book.comments;
+
+  const options = document.createElement("td");
+  const favBtn = document.createElement("button");
+  favBtn.classList.add("fav-btn");
+  const delBtn = document.createElement("button");
+  delBtn.classList.add("del-btn");
+
+  // Add buttons to table element
+  options.appendChild(favBtn);
+  options.appendChild(delBtn);
+
+  // Add elements to row
+  row.appendChild(author);
+  row.appendChild(title);
+  row.appendChild(pages);
+  row.appendChild(read);
+  row.appendChild(comments);
+  row.appendChild(options);
+
+  // Add row to table
+  tableBody.appendChild(row);
+};
+
+for (const book of myLibrary) {
+  displayBook(book);
+}
